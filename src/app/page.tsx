6f739d263ec2d5a43fc1b357f5e3351f8bb439ae
DanielCG <danielcgilibert@gmail.com>
2023-07-23 19:40:58 +0200

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className='flex-co flex min-h-screen items-center justify-between p-24'>
+      <div className='z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex'>
+        <h1>Template NextJS 13 with prettier</h1>
+      </div>
+    </main>
+  )
+}
